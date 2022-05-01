710c61c164d99bb293e736694116532d6fc3676d
Mert Safaktepe <safaktepemert@gmail.com>
2022-05-01 19:03:49 +0300

    Initial Commit

---
+++ News Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Application
+//
+//  Created by Mert Şafaktepe on 1.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
