e662e34f95835cdbc51ebc5ff312b622f0b7ec74
Mert Safaktepe <safaktepemert@gmail.com>
2022-05-03 16:28:31 +0300

    Custom Self-Sizing TableViewCells created. ViewModels implemented

---
+++ News Application/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  News Application
+//
+//  Created by Mert Şafaktepe on 3.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- News Application/Controllers/NewsListTableViewController.swift
+++ News Application/Controllers/NewsListTableViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 
 class NewsListTableViewController : UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +25,43 @@ class NewsListTableViewController : UITableViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
         let url = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2022-05-01&to=2022-05-01&sortBy=popularity&apiKey=f85840ca80884901aa13414ea9c2a1c3")!
-        
-        Webservices().getArticles(url: url) { _ in
+        /*
+        Webservices().getArticles(url: url) { articles in
             
+            self.articleListVM = ArticleListViewModel
+            
+            
+        }*/
+        Webservices().getArticles(url: url) { articles in
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                DispatchQueue.main.async{
+                    self.tableView.reloadData()
+                }
+             }
+         }
+     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numbersOfSection
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell NOT FOUND")
         }
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        
+        return cell
         
         
     }
+    
     
 }
